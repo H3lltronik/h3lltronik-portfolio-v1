936dbf58d00e5759a1545b4459e36b1f795250dd
H3lltronik <esau.egs1@gmail.com>
2023-05-02 14:16:46 -0600

    Changing server url to be a env variable

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const IMAGES_SERVER_URL = "";
-export const SERVER_URL = "https://h3lltronik-portfolio.herokuapp.com";
+export const SERVER_URL = process.env.SERVER_URL;
 export const SKIP_INITIAL_SEQUENCE = "skipInitialSequence";
 export const SEO_SITE_URL_BASE = "h3lltronik-portfolio-v1.vercel.app";
